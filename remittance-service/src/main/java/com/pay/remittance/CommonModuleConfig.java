f4cb536d8304d60bfeba6a326318fd330d3966fd
Huey-J <wkdgus7113@gmail.com>
2024-08-02 01:00:02 +0900

    chore: 송금 서버 추가

---
+++ remittance-service/src/main/java/com/pay/remittance/CommonModuleConfig.java
@@ -0,0 +1,10 @@
+package com.pay.remittance;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.pay.common")
+public class CommonModuleConfig {
+
+}
