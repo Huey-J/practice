95e9bb6635f197a38e4123e9301548980bd29448
Huey-J <wkdgus7113@gmail.com>
2024-08-15 23:02:22 +0900

    feat: MoneyServiceAdapter 구현

--- remittance-service/src/main/java/com/pay/remittance/adapter/out/service/money/MoneyServiceAdapter.java
+++ remittance-service/src/main/java/com/pay/remittance/adapter/out/service/money/MoneyServiceAdapter.java
@@ -1,5 +1,6 @@
 package com.pay.remittance.adapter.out.service.money;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pay.common.CommonHttpClient;
 import com.pay.common.ExternalSystemAdapter;
 import com.pay.remittance.application.port.out.money.MoneyInfo;
@@ -11,14 +12,24 @@ import org.springframework.beans.factory.annotation.Value;
 @RequiredArgsConstructor
 public class MoneyServiceAdapter implements MoneyPort {
 
-  private final CommonHttpClient moneyServiceHttpClient;
+  private final CommonHttpClient httpClient;
 
   @Value("${service.money.url}")
   private String moneyServiceEndpoint;
 
   @Override
   public MoneyInfo getMoneyInfo(Long membershipId) {
-    return null;
+    String url = String.join("/", moneyServiceEndpoint, "money", "membership", membershipId.toString());
+
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+
+      String responseBody = httpClient.sendGetRequest(url).body();
+
+      return objectMapper.readValue(responseBody, MoneyInfo.class);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
